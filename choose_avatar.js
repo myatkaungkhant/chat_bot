968712c80e3dd82b13a57fe274f9f5ba628b166a
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2022-03-17 12:03:26 +0630

    update chat bot

--- choose_avatar.js
+++ choose_avatar.js
@@ -1,6 +1,6 @@
 let pics = ["robot_1.png", "robot_2.png", "robot_3.png", "robot_4.png"];
 let choice;
-
+// from first to second 
 var go_click = document.getElementById("btn_next");
 go_click.addEventListener("click",() => {
     document.getElementById("display_1").style.display = "none";
@@ -12,9 +12,15 @@ go_click.addEventListener("click",() => {
 function check(obj) {
     choice = obj.id;
 }
-
+// from second to third 
 var skip_click = document.getElementById("btn_skip");
 skip_click.addEventListener("click",() => {
     document.getElementById("display_2").style.display = "none";
     document.getElementById("display_3").style.display = "block";
+});
+// sent message
+var sent_click = document.getElementById("sent_icon");
+sent_click.addEventListener("click",() => {
+    document.getElementById("user").style.display = "flex";
+    document.getElementById("user").innerHTML += document.getElementById("message").value;
 });
