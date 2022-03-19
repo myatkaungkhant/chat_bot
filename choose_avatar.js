1f87b208e5d49891013c6f903747ef53b0b2cd13
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2022-03-19 13:40:48 +0630

    update bot

--- choose_avatar.js
+++ choose_avatar.js
@@ -19,30 +19,48 @@ skip_click.addEventListener("click", () => {
     document.getElementById("display_3").style.display = "block";
 });
 // sent message
-let bot_message = ["Hello!", "I'm fine... How about you?", "Exciting things!", "Glad to hear it","How can I help you!","Ahh... I have special things for you!","Do you like to play music or game!", "I don't understand"];
+let bot_message = ["Hello!", "I'm fine... How about you?", "Exciting things!", "Glad to hear it", "How can I help you!", "Ahh... I have special things for you!", "Do you like to play music or game!", "I don't understand"];
 var sent_click = document.getElementById("sent_icon");
 sent_click.addEventListener("click", () => {
     document.getElementById("chatbox").innerHTML += "<div class='user' style='display:flex;'>" + document.getElementById("message").value + "</div>";
     var user_message = document.getElementById("message").value;
     document.getElementById("message").value = "";
-    if (user_message.includes("Hello")) {
-        document.getElementById("chatbox").innerHTML += "<div class='bot' style='display:flex;'>" + bot_message[0] + "</div>";
-    } else if (user_message.includes("How")) {
-        document.getElementById("chatbox").innerHTML += "<div class='bot' style='display:flex;'>" + bot_message[1] + "</div>";
-    } else if (user_message.includes("What")) {
-        document.getElementById("chatbox").innerHTML += "<div class='bot' style='display:flex;'>" + bot_message[2] + "</div>";
-    } else if (user_message.includes("happy")) {
-        document.getElementById("chatbox").innerHTML += "<div class='bot' style='display:flex;'>" + bot_message[3] + "</div>";
-    }else if(user_message.includes("boring") || user_message.includes("lonely")){
-        document.getElementById("chatbox").innerHTML += "<div class='bot' style='display:flex;'>" + bot_message[4] + "</div>";
-    }else if(user_message.includes("know")){
-        document.getElementById("chatbox").innerHTML += "<div class='bot' style='display:flex;'>" + bot_message[5] + "</div>";
-    }else if(user_message.includes("tell")){
-        document.getElementById("chatbox").innerHTML += "<div class='bot' style='display:flex;'>" + bot_message[6] + "</div>";
-    }
+    setTimeout(() => {
+        if (user_message.includes("Hello")) {
+            document.getElementById("chatbox").innerHTML +=
+                "<div class='bot' style='display:flex;'>" + bot_message[0] + "</div>";
+        } else if (user_message.includes("How")) {
+            document.getElementById("chatbox").innerHTML +=
+                "<div class='bot' style='display:flex;'>" + bot_message[1] + "</div>";
+        } else if (user_message.includes("What")) {
+            document.getElementById("chatbox").innerHTML +=
+                "<div class='bot' style='display:flex;'>" + bot_message[2] + "</div>";
+        } else if (user_message.includes("happy")) {
+            document.getElementById("chatbox").innerHTML +=
+                "<div class='bot' style='display:flex;'>" + bot_message[3] + "</div>";
+        } else if (
+            user_message.includes("boring") ||
+            user_message.includes("lonely")
+        ) {
+            document.getElementById("chatbox").innerHTML +=
+                "<div class='bot' style='display:flex;'>" + bot_message[4] + "</div>";
+        } else if (user_message.includes("know")) {
+            document.getElementById("chatbox").innerHTML +=
+                "<div class='bot' style='display:flex;'>" + bot_message[5] + "</div>";
+        } else if (user_message.includes("tell")) {
+            document.getElementById("chatbox").innerHTML +=
+                "<div class='bot' style='display:flex;'>" + bot_message[6] + "</div>";
+        } else if(user_message.includes("play")){
+            play_music();
+        } else {
+            document.getElementById("chatbox").innerHTML +=
+                "<div class='bot' style='display:flex;'>" + bot_message[7] + "</div>";
+        }
+    }, 1500);
 });
-
+// play music
 function play_music(){
-    
+    document.getElementById("content").style.display = "block";
 }
+
 
